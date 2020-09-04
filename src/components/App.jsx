bd9230962a0932a98e9586983f6fc1b5d7f32f37
GuaxinimRadical <talisonf11@gmail.com>
2020-09-04 17:44:07 -0300

    Mudança da função de requisitar os dados da API para o componente App, com o componente Pokemon apenas recebendo os dados em props

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -11,42 +11,82 @@ class App extends React.Component{
 
         this.state = {
             nameSearching: '',
-            pokemons: []
+            pokemonsToShow: Array(15).fill().map((a,i)=> i+1), //[1,2,3,4],
+            pokemons: Array(200).fill({ name: null, id:null, types:[null,null] })
         }
 
-        this.savePokemons = this.savePokemons.bind(this)
+        this.requestApiForSavePokemons(200)
+
         this.setInputSearch = this.setInputSearch.bind(this)
     }
 
-    savePokemons(name, id, types){
-        this.setState({
-            pokemons: [ ...this.state.pokemons, {
-                name: name,
-                id: id,
-                types: types
-            }]
-        })
+    makeArray (start, end) {
+        const array = []
+        for (let i = start+1; i <= end; i++) {
+            array.push(i)
+        }
+        return array
+    }
+
+    requestApiForSavePokemons(pokemonsForRequest) {
+        let array = Array(pokemonsForRequest)
+        for(let id=1; id <= pokemonsForRequest; id++){
+            const url = `https://pokeapi.co/api/v2/pokemon/${id}`
+            const request = fetch(url).then( i => i.json() )
+        
+            request.then( pok => {
+                const dades = {
+                    name: pok.name || null,
+                    types: [ 
+                        pok.types[0].type.name || null, 
+                        pok.types[1] ? pok.types[1].type.name : pok.types[0].type.name
+                    ]
+                }
+                 return dades
+                })
+            .then( dades => {
+                array[id] = dades
+            })
+        }
+        console.log(this.state)
+        setTimeout( i => this.setState({ pokemons: array}), 2000)
+        //this.setState({ pokemons: array})
     }
 
     setInputSearch(event){
         const value = event.target.value
         this.setState({ nameSearching: value })
+        this.handleSearch()
+        //console.log(this.state.pokemonsToShow)
+    }
+
+    handleSearch(){
+        const nameInSearch = this.state.nameSearching
+        if(!nameInSearch) return
+
+        const pokemonsFiltered = this.state.pokemons.map(
+            (pokemon) => {
+                if(pokemon.name.includes(nameInSearch))
+                    return pokemon.id
+            }
+        )
+        .filter( i=> i )
+        
+        this.setState({
+            pokemonsToShow: pokemonsFiltered
+        })
+        this.updatePokemons()
     }
 
     render(){
-        const pokemonsRequested = 15
-        const pokemonsId = []
-    
-        for (let i = 1; i <= pokemonsRequested; i++) {
-            pokemonsId.push(i)
-        }
+        console.log(this.state)
         return (
             <div>
                 <header>
                     <SearchBar valueInputSearch={this.state.nameSearching} setInputSearch={this.setInputSearch} />
                 </header>
                 <section className='pokemons'> 
-                    {pokemonsId.map( ids =>  <Pokemon id={ids} savePokemons={this.savePokemons} />)}
+                    {this.state.pokemonsToShow.map(  ids =>  <Pokemon id={ids} name={this.state.pokemons[ids].name} types={this.state.pokemons[ids].types}/>)}         
                 </section>
             </div>
         )
--- src/components/Pokemon.jsx
+++ src/components/Pokemon.jsx
@@ -7,33 +7,15 @@ export default class Pokemon extends React.Component {
     constructor(props){
         super(props)
 
-        const urlForGetPokemon = `https://pokeapi.co/api/v2/pokemon/${props.id}`
-        this.requestApi(urlForGetPokemon)
-
         this.state = {
-            name: null,
-            types: [null,null]
+            name: this.props.name,
+            types: this.props.types 
         }
-    }
-
-    requestApi(url) {
-        const request = fetch(url).then( i => i.json() )
-    
-        request.then( pok => {
-            this.setState({ 
-                name: pok.name,
-                types: [
-                    pok.types[0].type.name,
-                    pok.types[1] ? pok.types[1].type.name : pok.types[0].type.name
-                ]
-            })
-            this.props.savePokemons(pok.name, this.props.id, pok.types.map(i=>i.type.name) )
-        })
     }
 
     verificationIfTypesIsRepeat(){
         const typeInSet = new Set()
-        this.state.types.forEach( element => typeInSet.add(element))
+        this.props.types.forEach( element => typeInSet.add(element))
 
         const finalArray = Array.from(typeInSet).map( t => <BlockType type={t} /> )
         return finalArray
@@ -43,19 +25,19 @@ export default class Pokemon extends React.Component {
     render(){
         return(
             <div className="card">
-                <div className = {`cardContent bg-color-${this.state.types[0]}`}>
+                <div className = {`cardContent bg-color-${this.props.types[0]}`}>
                     <img 
                         src= {`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/${this.props.id}.png` }
                         alt=""
                     /> 
 
-                    <h1>{String(this.state.name).toUpperCase()}</h1>
+                    <h1>{String(this.props.name).toUpperCase()}</h1>
 
                     <div className='blocksOfTypes'>
                         { this.verificationIfTypesIsRepeat() }
                     </div>
 
-                    <section className={`atributes bg-color-dark-${this.state.types[1]}`}>
+                    <section className={`atributes bg-color-dark-${this.props.types[1]}`}>
                         <ul>
                             <li>#<b>{this.props.id}</b></li>
                             <li>Kanto</li>
